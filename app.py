808d71aeca79f0b560044aabebe8b79ce9d087a7
XiaoChin <xiaochin02@gmail.com>
2023-07-02 21:34:07 +0800

    feat: add visualization in app

--- app.py
+++ app.py
@@ -70,11 +70,11 @@ def get_user_inputs():
     with col1:
         studytime = st.select_slider("Weekly Study Time",list(studytime_op.keys()),"2 to 5 hours")
         health = st.select_slider("Current Health Status", list(good_bad_op.keys()), "Good")  
-        g1 = st.number_input("First Period Grade", min_value= 0, max_value = 100, value = 50)
+        g1 = st.number_input("First Period Grade (%)", min_value= 0, max_value = 100, value = 50)
     with col2:
         goout = st.select_slider("Going Out with Friends", list(goout_op.keys()), 'Often')
         famrel = st.select_slider("Quality of Family Relatonsips",list(good_bad_op.keys()),"Normal")
-        g2 = st.number_input("Second Period Grade", min_value= 0, max_value = 100, value = 50)
+        g2 = st.number_input("Second Period Grade (%)", min_value= 0, max_value = 100, value = 50)
     
     df_pred = pd.DataFrame(data=np.zeros(shape=(1, len(features))), columns=features, dtype='float')
     df_pred['age'] = standardize_data('age', age)
@@ -90,15 +90,53 @@ def get_user_inputs():
 
     return df_pred
 
+def plot_G1_G3():
+    rc = {'figure.figsize':(5,5),
+        'axes.facecolor':'#0e1117',
+        'axes.edgecolor': '#0e1117',
+        'axes.labelcolor': 'white',
+        'figure.facecolor': '#0e1117',
+        'patch.edgecolor': '#0e1117',
+        'text.color': 'white',
+        'xtick.color': 'white',
+        'ytick.color': 'white',
+        'grid.color': 'grey',
+        'font.size' : 8,
+        'axes.labelsize': 12,
+        'xtick.labelsize': 12,
+        'ytick.labelsize': 12}
+    plt.rcParams.update(rc)
+    fig, ax = plt.subplots()
+    ax = sns.regplot(x=df['G1']*5, y=df['G3']*5, x_jitter=.1, color = '#f21111',scatter_kws={"color": "#f21111"},line_kws={"color": "#c2dbfc"})
+    ax.set(title='First Period Grade vs Final Grade', xlabel = 'First Period Grade (%)', ylabel = 'Final Grade (%)')
+    st.pyplot(fig)
 
-st.set_page_config(layout="wide")
-st.title("Predicting Student Performance in Secondary Education")
-df_pred = get_user_inputs()
+def plot_G2_G3():
+    fig, ax = plt.subplots()
+    ax = sns.regplot(x=df['G2']*5, y=df['G3']*5)
+    ax.set(title='Second Period Grade vs Final Grade', xlabel = 'Second Period Grade (%)', ylabel = 'Final Grade (%)')
+    st.pyplot(fig)
 
-model = joblib.load('linear_reg_model.joblib')
-value_pred = model.predict(df_pred)[0]*5 
-if value_pred < 0:
-    value_pred = 0
-pred = f"{value_pred:.2f}%"
-st.metric(label="The final grade score is estimated to be", value=pred)
+def main():
+    # st.set_page_config(layout="wide")
+    st.title("Predicting Student Performance in Secondary Education")
+    col1, col2 = st.columns(2,gap='large')
+    with col1: 
+        plot_G1_G3()
+    with col2:
+        plot_G2_G3()
+    st.markdown('First period and second period grade have a significant on the Final Grade.')
+    st.divider()
 
+    st.subheader("Student's information:")
+    df_pred = get_user_inputs()
+
+    model = joblib.load('linear_reg_model.joblib')
+    value_pred = model.predict(df_pred)[0]*5 
+    if value_pred < 0:
+        value_pred = 0
+    pred = f"{value_pred:.2f}%"
+    st.divider()
+    st.metric(label="The final grade score is estimated to be", value=pred)
+
+main()
---
+++ readMe.txt
@@ -0,0 +1,11 @@
+How to run:
+Method 1 - 
+Go to https://student-performance-prediction-xc.streamlit.app/. 
+
+Method 2 -
+1. Open command prompt and go to this folder directory.
+2. type: streamlit run app.py
+
+model.ipynb is the file to construct the prediction model.
+app.py is the file to run streamlit.
+student_data.csv is the dataset file.
